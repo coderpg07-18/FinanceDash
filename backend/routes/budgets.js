4d94cf901481e0975491213d0bfa564c20e230c4
Prince Gupta <hpvictus10022@gmail.com>
2026-05-09 22:15:30 +0530

    Updated routes

--- backend/routes/budgets.js
+++ backend/routes/budgets.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn } = require("../middleware");
-const c = require("../controllers/budget");
+const c = require("../controllers/Budget.js");
 
 router.get("/", isLoggedIn, wrapAsync(c.index));
 router.post("/", isLoggedIn, wrapAsync(c.create));
